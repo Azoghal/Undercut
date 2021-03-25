9dfa59989b954b4d7377d425260b4279977d2b31
Sam Beer <stb41@cam.ac.uk>
2021-03-25 17:52:42 +0000

    initial intellij commit

---
+++ src/UndercutSketch.java
@@ -0,0 +1,18 @@
+import processing.core.PApplet;
+
+public class UndercutSketch extends PApplet {
+
+    public static void main(String[] args) {
+        UndercutSketch us = new UndercutSketch();
+        String[] processingArgs = {"UndercutSketch"};
+        runSketch(processingArgs,us);
+    }
+
+    public void setup() {
+
+    }
+
+    public void draw() {
+
+    }
+}
